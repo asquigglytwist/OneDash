0d4a4a1f983f2bfa08a28ed8c6ef86e61ab2fd2d
asquigglytwist <36764747+asquigglytwist@users.noreply.github.com>
2018-04-07 17:54:59 +0530

    Externalizing inline scripts
    Externalizing inline JS code into a separate file.

---
+++ OneDash/scripts/detailshandler.js
@@ -0,0 +1,36 @@
+﻿function onBeforePrint() {
+    var ndAllDetails = document.getElementsByTagName("details"), len = ndAllDetails.length;
+    for (var i = 0; i < len; i++) {
+        ndAllDetails[i].setAttribute("open", "");
+    }
+}
+
+function onAfterPrint() {
+    var ndAllDetails = document.getElementsByTagName("details"), len = ndAllDetails.length;
+    for (var i = 0; i < len; i++) {
+        ndAllDetails[i].removeAttribute("open");
+    }
+    onInit();
+}
+
+function onInit() {
+    var ndCurrentProducts = document.getElementById("CurrentProducts").getElementsByTagName("details");
+    if (ndCurrentProducts.length > 0) {
+        var firstProduct = ndCurrentProducts[0];
+        firstProduct.setAttribute("open", "");
+        var ndVersions = firstProduct.getElementsByTagName("details");
+        if (ndVersions.length > 0) {
+            var firstVersion = ndVersions[0];
+            firstVersion.setAttribute("open", "");
+            var ndReleases = firstVersion.getElementsByTagName("details");
+            if (ndReleases.length > 0) {
+                var firstRelease = ndReleases[0];
+                firstRelease.setAttribute("open", "");
+            }
+        }
+    }
+}
+
+window.onload = onInit;
+window.onbeforeprint = onBeforePrint;
+window.onafterprint = onAfterPrint;
