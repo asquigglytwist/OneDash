6c5a91d4284561224ff496dc4ac724f1564d197a
asquigglytwist <36764747+asquigglytwist@users.noreply.github.com>
2018-04-07 18:48:06 +0530

    Constructing a proper DashBoard page
    Constructing a proper DashBoard page from the stored data from Product, Version & Release.  And including the code behind to construct the data for binding the UI to.

--- OneDash/DashBoard.aspx.cs
+++ OneDash/DashBoard.aspx.cs
@@ -4,7 +4,40 @@ using System.Web.UI;
 
 public partial class DashBoard : Page
 {
+    List<Product> Products;
+
+    private void LoadAllInfo()
+    {
+        var ProductCodeNames = Product.GetAllProductCodeNames();
+        Products = new List<Product>(ProductCodeNames.Count);
+        foreach (var prodCodeName in ProductCodeNames)
+        {
+            var Prod = Product.LoadFromFile(prodCodeName);
+            var VersionCodeNames = Prod.GetAllVersionCodes();
+            Prod.ProductVersions = new List<Version>(VersionCodeNames.Count);
+            foreach (var verCodeName in VersionCodeNames)
+            {
+                var Versn = Version.LoadFromFile(prodCodeName, verCodeName);
+                var ReleaseCodeNames = Versn.GetAllReleaseCodes(prodCodeName);
+                Versn.ProductVersionReleases = new List<Release>(ReleaseCodeNames.Count);
+                foreach (var releaseCodeName in ReleaseCodeNames)
+                {
+                    var Relse = Release.LoadFromFileWithBugList(prodCodeName, verCodeName, releaseCodeName);
+                    Versn.ProductVersionReleases.Add(Relse);
+                }
+                Prod.ProductVersions.Add(Versn);
+            }
+            Products.Add(Prod);
+        }
+    }
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        LoadAllInfo();
+        if(Products.Count > 0)
+        {
+            rpCurrentProducts.DataSource = Products;
+            rpCurrentProducts.DataBind();
+        }
     }
 }
